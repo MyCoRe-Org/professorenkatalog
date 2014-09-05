4659e36bfae6648f4c681e7768377549eff9e3e0
Robert Stephan <rsteph-de@users.noreply.github.com>
2014-09-05 15:19:40 +0000

    ongoing development "workspace" - create object
    git-svn-id: https://server.mycore.de/svn/professorenkatalog/trunk@30605 7089d063-bc14-496d-ac90-53de1872eb6c

--- professorenkatalog-base-module/src/main/java/org/mycore/activiti/workflows/create_object_simple/person/MCRWorkflowMgrPerson.java
+++ professorenkatalog-base-module/src/main/java/org/mycore/activiti/workflows/create_object_simple/person/MCRWorkflowMgrPerson.java
@@ -1,14 +1,21 @@
 package org.mycore.activiti.workflows.create_object_simple.person;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.StringReader;
 
 import org.jdom2.Document;
 import org.jdom2.input.SAXBuilder;
+import org.mycore.activiti.MCRActivitiUtils;
 import org.mycore.activiti.workflows.create_object_simple.MCRAbstractWorkflowMgr;
+import org.mycore.activiti.workflows.create_object_simple.MCRWorkflowMgr;
 import org.mycore.common.MCRException;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.datamodel.metadata.MCRObjectMetadata;
+import org.xml.sax.SAXParseException;
 
-public class MCRWorkflowMgrPerson extends MCRAbstractWorkflowMgr implements org.mycore.activiti.workflows.create_object_simple.MCRWorkflowMgr{
+public class MCRWorkflowMgrPerson extends MCRAbstractWorkflowMgr implements MCRWorkflowMgr{
 	private static final String DEFAULT_METADATA_XML =""
 	+"<metadata>"
     +"<box.surname class='MCRMetaLangText'>"
@@ -31,5 +38,26 @@ public class MCRWorkflowMgrPerson extends MCRAbstractWorkflowMgr implements org.
 		}
 	}
 
+	
+	/**
+	 * 
+	 * @param mcrObjID
+	 * @return null if correct, errormessage otherwise
+	 */
+	@Override
+	public String validate(MCRObjectID mcrObjID){
+		File wfFile = new File(MCRActivitiUtils.getWorkflowDirectory(mcrObjID), mcrObjID.toString() + ".xml");
+		try {
+			MCRObject mcrWFObj = new MCRObject(wfFile.toURI());
+		}
+		catch(SAXParseException e){
+			return "XML Error: "+e.getMessage();
+		}
+		catch(IOException e){
+			return "I/O-Error: "+e.getMessage();
+		}
+		return null;
+		
+	}
 	
 }
