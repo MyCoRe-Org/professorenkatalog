cdc66ac284783cabc4261f4d71f1baa78eee10db
Robert Stephan <robert.stephan@uni-rostock.de>
2021-12-21 23:43:29 +0100

    upda java dependencies for workflow classes

--- profkat-base-module/src/main/java/org/mycore/activiti/workflows/create_object_simple/person/MCRWorkflowMgrPerson.java
+++ profkat-base-module/src/main/java/org/mycore/activiti/workflows/create_object_simple/person/MCRWorkflowMgrPerson.java
@@ -1,44 +1,16 @@
 package org.mycore.activiti.workflows.create_object_simple.person;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.nio.file.Path;
 
-import org.jdom2.Document;
-import org.jdom2.input.SAXBuilder;
-import org.mycore.activiti.MCRActivitiUtils;
-import org.mycore.activiti.workflows.create_object_simple.MCRAbstractWorkflowMgr;
-import org.mycore.activiti.workflows.create_object_simple.MCRWorkflowMgr;
-import org.mycore.common.MCRException;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
-import org.mycore.datamodel.metadata.MCRObjectMetadata;
+import org.mycore.jspdocportal.common.bpmn.MCRBPMNUtils;
+import org.mycore.jspdocportal.common.bpmn.workflows.create_object_simple.MCRAbstractWorkflowMgr;
+import org.mycore.jspdocportal.common.bpmn.workflows.create_object_simple.MCRWorkflowMgr;
 import org.xml.sax.SAXParseException;
 
 public class MCRWorkflowMgrPerson extends MCRAbstractWorkflowMgr implements MCRWorkflowMgr{
-	private static final String DEFAULT_METADATA_XML =""
-	+"<metadata>"
-    +"<box.surname class='MCRMetaLangText'>"
-    +"  <surname inherited='0' form='plain'>Neue/r Professor/-in</surname>"
-    +"</box.surname>"
-    + "</metadata>";
-
-	
-	@Override
-	public MCRObjectMetadata getDefaultMetadata(String mcrBase) {
-		SAXBuilder sax = new SAXBuilder();
-		try{
-		  Document doc = sax.build(new StringReader(DEFAULT_METADATA_XML));
-		  MCRObjectMetadata mcrOMD = new MCRObjectMetadata();
-		  mcrOMD.setFromDOM(doc.getRootElement());
-		  return mcrOMD;
-		}
-		catch(Exception e){
-			throw new MCRException("Could not create default metadata", e);
-		}
-	}
-
-	
 	/**
 	 * 
 	 * @param mcrObjID
@@ -46,7 +18,7 @@ public class MCRWorkflowMgrPerson extends MCRAbstractWorkflowMgr implements MCRW
 	 */
 	@Override
 	public String validate(MCRObjectID mcrObjID){
-		Path wfFile = MCRActivitiUtils.getWorkflowObjectFile(mcrObjID);
+		Path wfFile = MCRBPMNUtils.getWorkflowObjectFile(mcrObjID);
 		try {
 			@SuppressWarnings("unused")
             MCRObject mcrWFObj = new MCRObject(wfFile.toUri());
@@ -60,5 +32,14 @@ public class MCRWorkflowMgrPerson extends MCRAbstractWorkflowMgr implements MCRW
 		return null;
 		
 	}
+
+    @Override
+    protected String getDefaultMetadataXML(String mcrBase) {
+        return "<metadata>"
+            +"<box.surname class='MCRMetaLangText'>"
+            +"  <surname inherited='0' form='plain'>Neue/r Professor/-in</surname>"
+            +"</box.surname>"
+            + "</metadata>";
+    }
 	
 }
